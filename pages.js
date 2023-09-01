9347f3e50bd8eb105023e2126aeeca4566650cb6
Muhammad Hamza <muhammad.hamza@eigertrading.com>
2023-09-01 16:08:05 +0100

    Implement gh pages and deploy

---
+++ pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+const pathname = `$(__dirname)/build`;
+const repoURL = "https://trao90.github.io/tayyab"
+
+ghpages.publish(
+    pathname,
+    {
+        branch: "main",
+        repo: repoURL,
+    },
+    (err) => {
+        if (err) console.log("Error: ", err);
+        else console.log("DEPLOY HO GAYA!!!!")
+    }
+);
